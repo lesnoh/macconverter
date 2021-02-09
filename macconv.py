7ee3afc221c4273078714ea9b592157d74964be8
Michael Honsel <michael.honsel@logata.com>
2021-02-09 13:46:20 +0100

    input validation for stdin

--- macconv.py
+++ macconv.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python
-__version__ = 0.1
-import sys,argparse
+__version__ = 0.2
+import sys
+import argparse
+import re
 
 parser = argparse.ArgumentParser(description='Transform MAC addresses into common formats')
-parser.add_argument('infile', default=sys.stdin, type=argparse.FileType('r'),nargs='?', help="stdin")
-parser.add_argument('-i','--input', metavar="00.11:33-ab:cdef", type=str, help="MAC address -:. Characters will be stripped")
-parser.add_argument('-f','--format',metavar="[linux,cisco,aruba]", type=str, help="Destination format. Blank generates all")
+parser.add_argument('infile', default=sys.stdin, type=argparse.FileType('r'),nargs='?', help="read MACs from stdin")
+parser.add_argument('-i','--input', metavar="00.11:33-ab:cdef", type=str, help="one MAC address")
+parser.add_argument('-f','--format',metavar="[linux,cisco,aruba]", type=str, help="Destination format")
 args = parser.parse_args()
 
-
 def strip_mac(original_mac):
-    result =  original_mac.translate({ord(i): None for i in ':.-\n'})
-    if len(result) == 12:
-        return result
-    else:
-        print (original_mac, "is no valid MAC address")
-        sys.exit(1)
+    result =  original_mac.translate({ord(i): None for i in ':.-\n\r'}).lower()
+    if re.match("[0-9a-f]{12}$", result):
+        if len(result) == 12:
+            return result
 
 def plain_to_cisco(mac_plain):
     cisco_mac = mac_plain[:4]+'.'+mac_plain[4:8]+'.'+mac_plain[8:]
@@ -29,10 +28,7 @@ def plain_to_aruba(mac_plain):
     aruba_mac = mac_plain[:6]+'-'+mac_plain[6:]
     return (aruba_mac)
 
-def main():
-    mac_plain = strip_mac(args.infile.read())
-    print (mac_plain)
-    #mac_plain = strip_mac(args.input)
+def convert(mac_plain):
     if args.format == "cisco":
         print (plain_to_cisco(mac_plain))
     elif args.format == "linux":
@@ -44,6 +40,25 @@ def main():
         print (plain_to_cisco(mac_plain))
         print (plain_to_linux(mac_plain))
         print (plain_to_aruba(mac_plain))
+
+
+def main():
+    got_input = None
+    if not sys.stdin.isatty():
+        got_input = True
+        for stdin_mac in args.infile:
+            mac_plain = strip_mac(stdin_mac)
+            if mac_plain:
+                convert(strip_mac(mac_plain))
+            else:
+                print (stdin_mac.strip(), "is no valid MAC")
+
+    if args.input: 
+        convert(strip_mac(args.input))
+        got_input = True
+    else:
+        if not got_input:
+            print ("no CLI input")
 
 if __name__ == "__main__":
     main()
